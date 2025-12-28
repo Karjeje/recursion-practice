6cd603496fa1685de271165112f54f1a336d4cf4
Karjeje <193631637+Karjeje@users.noreply.github.com>
2025-12-28 11:53:37 +0100

    Write recursive fibonacci functions

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -20,3 +20,44 @@ function fibs(n) {
 }
 
 console.log(fibs(2));
+
+// function fibsRec(n) {
+//   console.log("This was printed recursively");
+
+//   if (n === 0) {
+//     return [];
+//   }
+
+//   if (n === 1) {
+//     return [0];
+//   }
+
+//   if (n === 2) {
+//     return [0, 1];
+//   }
+
+//   const prev = fibsRec(n - 1);
+
+//   const firstNum = prev[prev.length - 2];
+//   const secondNum = prev[prev.length - 1];
+
+//   const newNum = firstNum + secondNum;
+
+//   prev.push(newNum);
+
+//   return prev;
+// }
+
+function fibsRec(n) {
+  if (n === 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const prev = fibsRec(n - 1);
+
+  const nextNumber = prev[prev.length - 1] + prev[prev.length - 2];
+
+  return [...prev, nextNumber];
+}
+
+console.log(fibsRec(8));
