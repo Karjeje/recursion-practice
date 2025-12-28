ea616570f8235d923bab5037c7619557da942d82
Karjeje <193631637+Karjeje@users.noreply.github.com>
2025-12-28 13:00:02 +0100

    Write the helper function for merging arrays

--- src/mergesort.js
+++ src/mergesort.js
@@ -12,4 +12,32 @@ function mergeSort(arr) {
   return result;
 }
 
+function mergeHelper(arr1, arr2) {
+  const mergedArr = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      mergedArr.push(arr1[i]);
+      i++;
+    } else {
+      mergedArr.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    mergedArr.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    mergedArr.push(arr2[j]);
+    j++;
+  }
+
+  return mergedArr;
+}
+
 console.log(mergeSort([105, 79, 100, 110]));
